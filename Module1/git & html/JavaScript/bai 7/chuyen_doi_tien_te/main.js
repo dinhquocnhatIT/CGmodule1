1d8b61b63dfa49ead9d64db81713757dc44649b7
dinhquocnhat <nhatd87@gmail.com>
2022-06-14 08:41:01 +0700

    change file change

--- Module1/git & html/JavaScript/bai 7/chuyen_doi_tien_te/main.js
+++ Module1/git & html/JavaScript/bai 7/chuyen_doi_tien_te/main.js
@@ -2,19 +2,20 @@ function convertCurrency(){
     let Amount = document.getElementById("amount").value;
     let fromCurrency = document.getElementById("from_currency").value;
     let toCurrency = document.getElementById("to_currency").value;
-    let result;
-    if (fromCurrency == "VND" && toCurrency == "USD") {
-        result = 'result:' + (Amount / 23000) + 'USD';
+    let Result;
+    if (fromCurrency == "USD" && toCurrency == "VND") {
+        Result = 'result:' + (Amount * 23000) + 'Đ';
     }
-    else if (fromCurrency == "USD" && toCurrency == "VND") {
-        result = 'result:' + (Amount * 23000) + 'VND';
+    else if (fromCurrency == "VND" && toCurrency == "USD") {
+        Result = 'result:' + (Amount / 23000) + '$';
 
     }
-    else if (fromCurrency == "VND" && toCurrency =="VND"){
-        result = 'result:' + Amount + 'VND';
+    else if (fromCurrency == "VND"){
+        Result = 'result:' + Amount + "Đ";
     }
     else {
-        result = 'result:' + Amount + 'USD';
+        Result = 'result:' + Amount + "$";
     }
-    document.getElementById("result").innerHTML=result;
+    console.log(Result);
+   document.getElementById("result").innerText=Result;
 }
